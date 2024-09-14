8f012a35921cec79f7a4f04da1ffbfeccbb94ed6
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2024-09-14 10:59:28 -0300

    #inputs

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,12 +2,15 @@ import { useState} from "react"
 
 function App() {
 
+  const[valueInput, setValueInput] = useState('')
   return ( 
     <div>
       <h1>Bomba de combustível</h1>
       <div>
-        <label>Valor da Gasolina</label>
-        <input></input>
+        <label>Valor da Gasolina</label><br></br>
+        <input placeholder="Informe o valor da gasolina..."></input><br></br><br></br>
+        <label>Valor do Etanol</label><br></br>
+        <input placeholder="Informe o valor do etanol..."></input>
       </div>
    </div>
   )
