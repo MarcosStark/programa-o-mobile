c2ee14e25140b9abdb66188d04bdcff4abe32189
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2024-09-14 11:53:10 -0300

    #input_functions

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,15 +2,25 @@ import { useState} from "react"
 
 function App() {
 
-  const[valueInput, setValueInput] = useState('')
+  const[valueInput_petrol, setValueInput_petrol] = useState('')
+  const[valueInput_ethanol, setValueInput_ethanol] = useState('')
+
+  const onChangeInput_petrol = (event) => {
+    setValueInput_petrol(event.target.value)
+  }
+
+  const onChangeInput_ethanol = (event) => {
+    setValueInput_ethanol(event.target.value)
+  }
+
   return ( 
     <div>
       <h1>Bomba de combustível</h1>
       <div>
         <label>Valor da Gasolina</label><br></br>
-        <input placeholder="Informe o valor da gasolina..."></input><br></br><br></br>
+        <input onChange={onChangeInput_petrol} placeholder="Informe o valor da gasolina..."></input><br></br><br></br>
         <label>Valor do Etanol</label><br></br>
-        <input placeholder="Informe o valor do etanol..."></input>
+        <input onChange={onChangeInput_ethanol} placeholder="Informe o valor do etanol..."></input>
       </div>
    </div>
   )
